9549d0b480e2bb775d8a54c73e8a4222d70657a7
Roman Vlasenko <roof1rst@gmail.com>
2016-09-08 14:35:30 +0400

    Changed the text that users must replace with the currency code.

--- app/addons/default_secondary_currency/func.php
+++ app/addons/default_secondary_currency/func.php
@@ -3,5 +3,5 @@
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 function fn_default_secondary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency) {
-    $secondary_currency = 'DESIRED_ADMIN_CURRENCY_CODE';
+    $secondary_currency = 'DESIRED_STOREFRONT_CURRENCY_CODE';
 }
