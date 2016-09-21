64ebcec95d5b76f256707f4ae9f210201f729bfd
Roman <admiralexo@gmail.com>
2016-09-21 17:46:02 +0400

    [~] {@17401} Variable moved in the module settings.

--- app/addons/default_secondary_currency/func.php
+++ app/addons/default_secondary_currency/func.php
@@ -1,8 +1,10 @@
 <?php
 
+use Tygh\Registry;
+
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 function fn_default_secondary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency)
 {
-    $secondary_currency = 'DESIRED_STOREFRONT_CURRENCY_CODE';
+    $secondary_currency = Registry::get('addons.default_secondary_currency.storefront_code');
 }
