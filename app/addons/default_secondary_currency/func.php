9512eae51b479c0af559b1019c584ce6039fac35
Roman Vlasenko <roof1rst@gmail.com>
2016-09-21 11:18:12 +0400

    [~] {@17401} The curly brace is moved to the new line.

--- app/addons/default_secondary_currency/func.php
+++ app/addons/default_secondary_currency/func.php
@@ -2,6 +2,7 @@
 
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
-function fn_default_secondary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency) {
+function fn_default_secondary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency)
+{
     $secondary_currency = 'DESIRED_STOREFRONT_CURRENCY_CODE';
 }
