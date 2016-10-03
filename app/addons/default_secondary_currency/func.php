6884f5343ffcef21040357b1d98d455ede585add
Roman Vlasenko <roof1rst@gmail.com>
2016-10-03 17:11:20 +0400

    [~] {@17401} Added old secondary currency.

--- app/addons/default_secondary_currency/func.php
+++ app/addons/default_secondary_currency/func.php
@@ -7,6 +7,10 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 function fn_default_secondary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency)
 {
     if ($area == 'C') {
-        $secondary_currency = Registry::get('addons.default_secondary_currency.storefront_code');
+        $old_secondary_currency = $secondary_currency ?: null;
+
+        if ($old_secondary_currency != $secondary_currency) {
+            $secondary_currency = Registry::get('addons.default_secondary_currency.storefront_code');
+        }
     }
 }
