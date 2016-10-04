9311d837187ab3ddc1850b35b7e416165b1ffc22
Roman Vlasenko <roof1rst@gmail.com>
2016-10-04 16:09:26 +0400

    [~] {@17401} Removed old secondary currency.

--- app/addons/default_secondary_currency/func.php
+++ app/addons/default_secondary_currency/func.php
@@ -6,11 +6,13 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 function fn_default_secondary_currency_init_currency_post(&$params, &$area, &$primary_currency, &$secondary_currency)
 {
-    if ($area == 'C') {
-        $old_secondary_currency = $secondary_currency ?: null;
+    $storefront_code = Registry::get('addons.default_secondary_currency.storefront_code');
 
-        if ($old_secondary_currency != $secondary_currency) {
-            $secondary_currency = Registry::get('addons.default_secondary_currency.storefront_code');
+    if ($area == 'C') {
+        if (array_key_exists('currency', $params)) {
+            $secondary_currency = $params['currency'];
+        } else {
+            $secondary_currency = $storefront_code;
         }
     }
 }
