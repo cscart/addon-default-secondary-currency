e3096072aa91bc7dc988fc47d35f69742191f87b
Roman Vlasenko <roof1rst@gmail.com>
2016-10-04 18:56:47 +0400

    [~] {@17401} The method is changed to another.

--- app/addons/default_secondary_currency/func.php
+++ app/addons/default_secondary_currency/func.php
@@ -11,6 +11,9 @@ function fn_default_secondary_currency_init_currency_post(&$params, &$area, &$pr
     if ($area == 'C') {
         if (array_key_exists('currency', $params)) {
             $secondary_currency = $params['currency'];
+            Registry::set('addons.default_secondary_currency.currency', $params['currency']);
+        } elseif (Registry::isExist('addons.default_secondary_currency.currency')) {
+            $secondary_currency = Registry::get('addons.default_secondary_currency.currency');
         } else {
             $secondary_currency = $storefront_code;
         }
